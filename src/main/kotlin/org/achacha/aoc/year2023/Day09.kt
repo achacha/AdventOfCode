c31d1854487a9b36877ab34d0ae61f20f51bf64a
Alex Chachanashvili <achacha@hotmail.com>
2023-12-31 09:40:54 -0600

    Day09 part1

---
+++ src/main/kotlin/org/achacha/aoc/year2023/Day09.kt
@@ -0,0 +1,54 @@
+package org.achacha.aoc.year2023
+
+/**
+ * https://adventofcode.com/2023/day/9
+ */
+class Day09 {
+
+    fun part1(input: String): Long {
+        return parseInput(input)
+            .mapIndexed { lino, linenums ->
+                val sequenceArray = processLinePart1(linenums)
+
+                // Sum up last of each line from end
+                var result = 0L
+                for (i in sequenceArray.size-1 downTo 0 step 1) {
+                    result += sequenceArray[i].last()
+                }
+//                println("line[$lino]=$result")
+                result
+            }
+            .sum()
+    }
+
+    fun parseInput(input: String): List<List<Int>> {
+        return input.lines().map { line ->
+            line.split(" ").filter(String::isNotBlank).map(String::toInt)
+        }
+    }
+
+    fun processLinePart1(nums: List<Int>): List<List<Int>> {
+//        println("PROCESS: $nums")
+        val sequenceArray = mutableListOf<List<Int>>()
+        var currentRow = nums
+        while (true) {
+            sequenceArray.add(currentRow)
+            val nextRow = getNextDiffRow(currentRow)
+
+//            println("NEXT: $nextRow")
+
+            if (nextRow.all { it == 0 }) break
+            else currentRow = nextRow
+
+        }
+        return sequenceArray
+    }
+
+    fun getNextDiffRow(input: List<Int>): List<Int> {
+        val output = mutableListOf<Int>()
+        for (i in 0 ..< input.size-1) {
+            output.add(input[i+1] - input[i])
+        }
+        return output
+    }
+}
