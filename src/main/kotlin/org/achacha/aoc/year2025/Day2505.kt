f1eefbe7d7f0c88bb002500785555bb25b0d0af1
Alex Chachanashvili <achacha@hotmail.com>
2025-12-08 13:46:31 -0600

    Day 2505 part 2

--- src/main/kotlin/org/achacha/aoc/year2025/Day2505.kt
+++ src/main/kotlin/org/achacha/aoc/year2025/Day2505.kt
@@ -1,11 +1,14 @@
 package org.achacha.aoc.year2025
 
 import org.achacha.common.load2StringLines
+import org.apache.commons.lang3.tuple.MutablePair
+import kotlin.math.max
+import kotlin.math.min
 
 class Day2505 {
     class InputData {
-        val fresh : MutableList<Pair<Long,Long>> = mutableListOf()
-        val produce: MutableSet<Long> = mutableSetOf()
+        var fresh : MutableList<MutablePair<Long,Long>> = mutableListOf()
+        var produce: MutableSet<Long> = mutableSetOf()
     }
 
     fun parseInputData(resourcePath: String): InputData {
@@ -19,7 +22,7 @@ class Day2505 {
             if (line.isEmpty()) break  // Done with range
 
             line.split("-").let {
-                result.fresh.add(Pair(it[0].toLong(), it[1].toLong()))
+                result.fresh.add(MutablePair(it[0].toLong(), it[1].toLong()))
             }
         }
 
@@ -36,13 +39,82 @@ class Day2505 {
         var result = 0
         produceLoop@for (productId in inputData.produce) {
             for (f in inputData.fresh) {
-                if (productId >= f.first && productId <= f.second) {
-                    println("Product $productId found in ${f.first} ${f.second}")
+                if (productId >= f.left && productId <= f.right) {
+                    println("Product $productId found in ${f.left} ${f.right}")
                     result++
                     continue@produceLoop
                 }
             }
         }
+        return result
+    }
+
+    /**
+     * Return how mny ranges were compacted
+     */
+    fun compactRanges(inputData: InputData): Int {
+        var result = 0
+
+        var i = 0
+        while (i < inputData.fresh.size) {
+            assert(inputData.fresh[i].left <= inputData.fresh[i].right)
+            for (j in i+1 until inputData.fresh.size) {
+                // No intersection, do nothing
+                if (
+                    inputData.fresh[i].right < inputData.fresh[j].left
+                    || inputData.fresh[j].right < inputData.fresh[i].left
+                ) continue
+
+                // overlap
+                else if (
+                    inputData.fresh[i].right >= inputData.fresh[j].left
+                    || inputData.fresh[i].left <= inputData.fresh[j].right
+                ) {
+                    inputData.fresh[i].left = min(inputData.fresh[i].left, inputData.fresh[j].left)
+                    inputData.fresh[i].right = max(inputData.fresh[j].right, inputData.fresh[i].right)
+                    inputData.fresh[j].left = -1L
+                    inputData.fresh[j].right = -1L
+                }
+
+                // contained
+                else if (
+                    (inputData.fresh[i].left <= inputData.fresh[j].left && inputData.fresh[i].right <= inputData.fresh[j].right)
+                    || (inputData.fresh[j].left <= inputData.fresh[i].left && inputData.fresh[j].right <= inputData.fresh[i].right)
+                ) {
+                    inputData.fresh[i].left = min(inputData.fresh[i].left, inputData.fresh[j].left)
+                    inputData.fresh[i].right = max(inputData.fresh[j].right, inputData.fresh[i].right)
+                    inputData.fresh[j].left = -1L
+                    inputData.fresh[j].right = -1L
+                }
+            }
+
+            // compact and remove (-1,-1)
+            val initSize = inputData.fresh.size
+            //println("PreCompact: i=$i  ranges=${inputData.fresh}")
+            inputData.fresh = inputData.fresh.filter { it.left != -1L }.toMutableList()
+            if (inputData.fresh.size < initSize) {
+                i = 0
+                result++
+            } else i++
+            //println("PostCompact: i=$i  ranges=${inputData.fresh}")
+        }
+
+        return result
+    }
+
+    fun part2(resourcePath: String): Long {
+        val inputData = parseInputData(resourcePath)
+        var result = 0L
+
+        while (compactRanges(inputData) > 0) {
+            print(".")
+        }
+
+        // Add all values in compacted ranges
+        for (range in inputData.fresh) {
+            result += (range.right - range.left + 1)
+        }
+
         return result
     }
 }
--- src/test/kotlin/org/achacha/aoc/year2025/Day2505Test.kt
+++ src/test/kotlin/org/achacha/aoc/year2025/Day2505Test.kt
@@ -1,5 +1,6 @@
 package org.achacha.aoc.year2025
 
+import org.apache.commons.lang3.tuple.MutablePair
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
@@ -18,13 +19,41 @@ class Day2505Test {
     }
 
     @Test
-    fun part2sample() {
+    fun part1sample() {
         assertEquals(3, Day2505().part1("/year2025/day2505_sample.txt"))
     }
 
     @Test
-    fun part2() {
+    fun part1() {
         assertEquals(690, Day2505().part1("/year2025/day2505.txt"))
+    }
+
+    @Test
+    fun compactRanges() {
+        val inputData = Day2505.InputData()
+        inputData.fresh = mutableListOf(
+            MutablePair(1L,4L),
+            MutablePair(2L,5L),
+            MutablePair(8L,10L),
+            MutablePair(10L,12L),
+            MutablePair(20L,25L),
+            MutablePair(20L,25L),
+            MutablePair(22L,23L),
+            MutablePair(30L,32L),
+            MutablePair(29L,33L)
+        )
+
+        assertEquals(4, Day2505().compactRanges(inputData))
+    }
+
+    @Test
+    fun part2sample() {
+        assertEquals(14, Day2505().part2("/year2025/day2505_sample.txt"))
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(344323629240733, Day2505().part2("/year2025/day2505.txt"))
     }
 
 }
